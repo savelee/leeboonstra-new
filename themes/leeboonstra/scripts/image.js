62c40d2e803992290e721ce2ff488580b717be67
Lee Boonstra <lee@leeboonstra.com>
2021-02-11 16:44:13 +0100

    speed fixes

--- themes/leeboonstra/scripts/image.js
+++ themes/leeboonstra/scripts/image.js
@@ -5,13 +5,13 @@ hexo.extend.filter.register('after_post_render', data => {
     var regex = new RegExp(/(<img\b[^<>]*?src=['"].*?\/?.*?)([^.\/<>]*)(\.[^.\/<>]*?['"])([^><]*?\>)/igm);
     data.content = data.content.replace(regex, (_, start, name, ext, end) => {
       return `<picture>
-        <source media="(min-width: 36em)"
-        srcset="/images/large_${name}${ext} 1024w,
-                /images/medium_${name}${ext} 640w,
-                /images/small_${name}${ext}  320w"
-            sizes="33.3vw" />
+        <source srcset="/images/large_${name}${ext}" media="(min-width: 1000px)">
+        <source srcset="/images/medium_${name}${ext}" media="(min-width: 500px)">
+        <source srcset="/images/small_${name}${ext} " media="(max-width: 499px)">
         <img src="/images/small_${name}${ext}" ${end}
         </picture>`;
     });
 });
+
+
 
