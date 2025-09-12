3b8992171b05ce45ca4a579ce07e5672a828e1d3
Lee Boonstra <me@leeboonstra.com>
2025-09-12 09:56:58 +0200

    temp fix

--- hexo-images/lib/utils/generateFileName.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-const generateFileName = (basePath, hash, size, ext) =>
-    `${hash}/${path
-        .basename(basePath, path.extname(basePath))
-        .replace(/\.noresize$/gi, '')
-        .replace(/\@cover\./gi, '.')}@${size}.${ext}`.toLowerCase();
-
-module.exports = { generateFileName };
